325a33f56b9a7c38d7610faa0ff31fbe0d14b92c
dcrudup <denise@crudup.net>
2018-05-08 11:10:08 -0400

    Update2

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -16,13 +16,13 @@ public class FortuneTeller {
 		System.out.println("Hello " + firstName + " " + lastName + "." + " How old are you?");
 		int currentAge = input.nextInt();
 
-		// Retirement Years
 		int retirementNumber;
+
 		if (currentAge % 2 == 0)
-			// System.out.println("You can retire in 10 years.");
+			// Retire in 10 years
 			retirementNumber = 10;
 		else
-			// System.out.println("You can retire in 20 years.");
+			// Retire in 20 years
 			retirementNumber = 20;
 
 		// Bank Balance
@@ -32,7 +32,7 @@ public class FortuneTeller {
 		System.out.println("In which month were you born? Please enter the month using a number 1-12.");
 		int birthMonth = input.nextInt();
 
-		if (birthMonth > 1) {
+		if (birthMonth < 1) {
 			bankBalance = "$0.00";
 
 		} else if (birthMonth <= 4) {
@@ -43,20 +43,22 @@ public class FortuneTeller {
 			bankBalance = "$3,687,105.42";
 
 		} else if (birthMonth <= 12) {
-
 			bankBalance = "$86.23";
-
-		} else if (birthMonth > 12) {
-			bankBalance = "$0.00";
 		}
 
 		// ROYGBIV and Modes of Transportation
 		System.out.println(
 				"Please enter one of the ROYGBIV colors. If you're not sure which colors are ROYGBIV, please enter the word, help");
+
 		String roygbiv = input.next();
 		String modeOfTransportation = "";
 
-		if (roygbiv.equals("red")) {
+		if (roygbiv.equalsIgnoreCase("Help")) {
+			System.out.println("Red; Orange; Yellow; Green; Blue; Indigo; Violet. Please enter your chosen color.");
+			roygbiv = input.next();
+		}
+
+		if (roygbiv.equalsIgnoreCase("red")) {
 			modeOfTransportation = "Maserati";
 
 		} else if (roygbiv.equalsIgnoreCase("orange")) {
@@ -76,20 +78,41 @@ public class FortuneTeller {
 
 		} else if (roygbiv.equalsIgnoreCase("violet")) {
 			modeOfTransportation = "flying saucer";
+		}
 
-		} else if (roygbiv.equalsIgnoreCase("Help")) {
-			System.out.println("Red; Orange; Yellow; Green; Blue; Indigo; Violet");
+		// Vacation Home
+		double numberOfSiblings = 0;
+		String vacationHome = "";
 
-			System.out.println(
-					"Please enter one of the ROYGBIV colors. If you're not sure which colors are ROYGBIV, please enter the word, help");
-			roygbiv = input.next();
-			System.out.println("Thank you.");
+		System.out.println("How many siblings do you have?");
+		numberOfSiblings = input.nextDouble();
+
+		if (numberOfSiblings < 0) {
+			vacationHome = "Chernobyl, Ukraine";
+
+		} else if (numberOfSiblings == 0) {
+			vacationHome = "Boca Raton, FL";
+
+		} else if (numberOfSiblings == 1) {
+			vacationHome = "Nassau, Bahamas";
+
+		} else if (numberOfSiblings == 2) {
+			vacationHome = "Ponta Negra, Brazil";
+
+		} else if (numberOfSiblings == 3) {
+			vacationHome = "Portland, Oregon";
+
+		} else if (numberOfSiblings > 3) {
+			vacationHome = "Baton Rouge, LA";
 		}
 
 		input.close();
+		
+		System.out.println("");
 
-		System.out.println(firstName + " " + lastName + " " + "will retire in " + " " + "with a bank balance of "
-				+ bankBalance + " " + ", and will travel by " + modeOfTransportation);
+		System.out.println(firstName + " " + lastName + " " + "will retire in " + retirementNumber + " "
+				+ "years, with a bank balance of " + bankBalance + ", in a vacation home in " + vacationHome
+				+ ", and will travel by " + modeOfTransportation + ".");
 	}
 
 }
--- src/VacationHome.java
+++ src/VacationHome.java
@@ -7,27 +7,28 @@ public class VacationHome {
 		Scanner input = new Scanner(System.in);
 
 		double numberOfSiblings = 0;
+		String vacationHome = "";
 
 		System.out.println("How many siblings do you have?");
 		numberOfSiblings = input.nextDouble();
 
 		if (numberOfSiblings < 0) {
-			System.out.println("Chernobyl, Ukraine");
+			vacationHome = "Chernobyl, Ukraine";
 
 		} else if (numberOfSiblings == 0) {
-			System.out.println("Boca Raton, FL");
+			vacationHome = "Boca Raton, FL";
 
 		} else if (numberOfSiblings == 1) {
-			System.out.println("Nassau, Bahamas");
+			vacationHome = "Nassau, Bahamas";
 
 		} else if (numberOfSiblings == 2) {
-			System.out.println("Ponta Negra,Brazil");
+			vacationHome = "Ponta Negra,Brazil";
 
 		} else if (numberOfSiblings == 3) {
-			System.out.println("Portland, Oregon");
+			vacationHome = "Portland, Oregon";
 
 		} else if (numberOfSiblings > 3) {
-			System.out.println("Baton Rouge, LA");
+			vacationHome = "Baton Rouge, LA";
 		}
 
 		input.close();
